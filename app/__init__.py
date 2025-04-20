75b2a177255b924705ab32efc2ac1224b877efc4
Anusha Pariti <anusha.pariti@student.uts.edu.au>
2025-04-20 18:10:23 +0800

    CRUD API for payment_msgs_raw

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
-from app.api.routes_auth import auth_blueprint
+from app.api.routes_auth import auth_blueprint,payment_blueprint
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(auth_blueprint, url_prefix='/api')
+    app.register_blueprint(auth_blueprint, url_prefix='/api/auth')
+    app.register_blueprint(payment_blueprint, url_prefix='/api/payment')
     return app
