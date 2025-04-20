0ae4f0878e51f3a29488802518be70008e1d82ba
Anusha Pariti <anusha.pariti@student.uts.edu.au>
2025-04-20 18:05:05 +0800

    CRUD API for payment_msgs_raw

--- app/api/routes_auth.py
+++ app/api/routes_auth.py
@@ -2,6 +2,6 @@ from flask import Blueprint, jsonify
 
 auth_blueprint = Blueprint('auth', __name__)
 
-@auth_blueprint.route('/ping', methods=['GET'])
+@auth_blueprint.route('/auth/ping', methods=['GET'])
 def ping():
     return jsonify({'message': 'Auth service is up'}), 200
--- app/api/routes_payment.py
+++ app/api/routes_payment.py
@@ -9,24 +9,24 @@ from app.services.payment_service import (
 
 payment_blueprint = Blueprint('payment', __name__)
 
-@payment_blueprint.route('/', methods=['GET'])
+@payment_blueprint.route('/payment/', methods=['GET'])
 def list_payments():
     return jsonify(get_all_payments())
 
-@payment_blueprint.route('/<string:message_id>', methods=['GET'])
+@payment_blueprint.route('/payment/<string:message_id>', methods=['GET'])
 def get_payment(message_id):
     return jsonify(get_payment_by_id(message_id))
 
-@payment_blueprint.route('/', methods=['POST'])
+@payment_blueprint.route('/payment/', methods=['POST'])
 def add_payment():
     data = request.get_json()
     return jsonify(create_payment(data)), 201
 
-@payment_blueprint.route('/<string:message_id>', methods=['PUT'])
+@payment_blueprint.route('/payment/<string:message_id>', methods=['PUT'])
 def modify_payment(message_id):
     data = request.get_json()
     return jsonify(update_payment(message_id, data))
 
-@payment_blueprint.route('/<string:message_id>', methods=['DELETE'])
+@payment_blueprint.route('/payment/<string:message_id>', methods=['DELETE'])
 def remove_payment(message_id):
     return jsonify(delete_payment(message_id))
